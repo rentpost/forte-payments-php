737c21c01fa11f552383ea830e8df95a5560d55f
Jacob Thomason <jacob@thomason.xxx>
2022-08-19 22:56:38 -0400

    Removed decimal logic for Money normalization as Forte is resolving the root issue on their systems

--- src/Attribute/Money.php
+++ src/Attribute/Money.php
@@ -12,20 +12,6 @@ class Money extends Decimal
      */
     protected function deinternalize($internalizedValue): ?string
     {
-        $amount = (string)$internalizedValue;
-        $amountParts = explode('.', $amount);
-        if (count($amountParts) === 2) {
-            // x.00
-            if ($amountParts[1] == 0) {
-                return number_format((float)$amount, 0, '.', '');
-            }
-
-            // x.50 return as x.5
-            if (\str_ends_with($amountParts[1], '0')) {
-                return number_format((float)$amount, 1, '.', '');
-            }
-        }
-
-        return number_format((float)$amount, 2, '.', '');
+        return number_format((float)$internalizedValue, 2, '.', '');
     }
 }
