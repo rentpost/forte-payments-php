a084858119c3d8a7203e5f8bc0103247d6ea9154
Jacob Thomason <jacob@thomason.xxx>
2023-03-04 18:29:59 -0500

    CS fixes

--- src/Model/Echeck.php
+++ src/Model/Echeck.php
@@ -4,74 +4,39 @@ declare(strict_types = 1);
 
 namespace Rentpost\ForteApi\Model;
 
-use Rentpost\ForteApi\Attribute as Attribute;
+use Rentpost\ForteApi\Attribute;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * Echeck model
- * 
+ *
  * @author Jacob Thomason <jacob@rentpost.com>
  */
 class Echeck extends AbstractModel
 {
 
-    /**
-     * @var string
-     */
-    protected $accountHolder;
+    protected ?string $accountHolder;
 
-    /**
-     * api read only field
-     * @var string
-     */
-    protected $last4AccountNumber;
+    // Read-only field
+    protected ?string $last4AccountNumber;
 
-    /**
-     * @var string
-     */
-    protected $accountNumber;
+    protected ?Attribute\BankAccountNumber $accountNumber;
+    protected ?Attribute\BankRoutingNumber $routingNumber;
 
-    /**
-     * @var Attribute\BankRoutingNumber
-     */
-    protected $routingNumber;
-
-    /**
-     * @var string
-     */
     #[Assert\Choice(['Checking', 'Savings'])]
-    protected $accountType;
+    protected ?string $accountType;
 
-    /**
-     * @var string
-     */
-    protected $itemDescription;
-
-    /**
-     * @var string
-     */
-    protected $secCode;
-
-    /**
-     * @var string
-     */
-    protected $oneTimeToken;
+    protected ?string $itemDescription;
+    protected ?string $secCode;
+    protected ?Attribute\Id\OneTimeToken $oneTimeToken;
 
 
-    /**
-     * @return string
-     */
     public function getAccountHolder(): ?string
     {
         return $this->accountHolder;
     }
 
 
-    /**
-     * @param string $accountHolder
-     *
-     * @return self
-     */
     public function setAccountHolder(string $accountHolder): self
     {
         $this->accountHolder = $accountHolder;
@@ -80,9 +45,6 @@ class Echeck extends AbstractModel
     }
 
 
-    /**
-     * @return string
-     */
     public function getLast4AccountNumber(): ?string
     {
         return $this->last4AccountNumber;
@@ -91,10 +53,6 @@ class Echeck extends AbstractModel
 
     /**
      * @internal api read only field
-     *
-     * @param string $last4AccountNumber
-     *
-     * @return self
      */
     public function setLast4AccountNumber(string $last4AccountNumber): self
     {
@@ -104,20 +62,12 @@ class Echeck extends AbstractModel
     }
 
 
-    /**
-     * @return Attribute\BankAccountNumber
-     */
     public function getAccountNumber(): ?Attribute\BankAccountNumber
     {
         return $this->accountNumber;
     }
 
 
-    /**
-     * @param Attribute\BankAccountNumber $accountNumber
-     *
-     * @return self
-     */
     public function setAccountNumber(Attribute\BankAccountNumber $accountNumber): self
     {
         $this->accountNumber = $accountNumber;
@@ -126,20 +76,12 @@ class Echeck extends AbstractModel
     }
 
 
-    /**
-     * @return Attribute\BankRoutingNumber
-     */
     public function getRoutingNumber(): ?Attribute\BankRoutingNumber
     {
         return $this->routingNumber;
     }
 
 
-    /**
-     * @param Attribute\BankRoutingNumber $routingNumber
-     *
-     * @return self
-     */
     public function setRoutingNumber(Attribute\BankRoutingNumber $routingNumber): self
     {
         $this->routingNumber = $routingNumber;
@@ -148,20 +90,12 @@ class Echeck extends AbstractModel
     }
 
 
-    /**
-     * @return string
-     */
     public function getAccountType(): ?string
     {
         return $this->accountType;
     }
 
 
-    /**
-     * @param string $accountType
-     *
-     * @return self
-     */
     public function setAccountType(string $accountType): self
     {
         $this->accountType = $accountType;
@@ -170,20 +104,12 @@ class Echeck extends AbstractModel
     }
 
 
-    /**
-     * @return string
-     */
     public function getItemDescription(): ?string
     {
         return $this->itemDescription;
     }
 
 
-    /**
-     * @param string $itemDescription
-     *
-     * @return self
-     */
     public function setItemDescription(string $itemDescription): self
     {
         $this->itemDescription = $itemDescription;
@@ -192,20 +118,12 @@ class Echeck extends AbstractModel
     }
 
 
-    /**
-     * @return string
-     */
     public function getSecCode(): ?string
     {
         return $this->secCode;
     }
 
 
-    /**
-     * @param string $secCode
-     *
-     * @return self
-     */
     public function setSecCode(string $secCode): self
     {
         $this->secCode = $secCode;
@@ -214,27 +132,16 @@ class Echeck extends AbstractModel
     }
 
 
-    /**
-     * @return Attribute\Id\OneTimeToken
-     */
     public function getOneTimeToken(): ?Attribute\Id\OneTimeToken
     {
         return $this->oneTimeToken;
     }
 
 
-    /**
-     * @param Attribute\Id\OneTimeToken  $oneTimeToken
-     *
-     * @return self
-     */
     public function setOneTimeToken(Attribute\Id\OneTimeToken $oneTimeToken): self
     {
         $this->oneTimeToken = $oneTimeToken;
 
         return $this;
     }
-
-
-
 }
