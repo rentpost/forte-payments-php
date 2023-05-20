3dc6dd5adeee60b4f508553fa4331a01745b5e35
Jacob Thomason <jacob@thomason.xxx>
2023-05-20 01:39:39 -0400

    Added new method to find all settlements for a given transaction ID

--- src/Client/SubResource/SettlementSubResource.php
+++ src/Client/SubResource/SettlementSubResource.php
@@ -4,6 +4,9 @@ declare(strict_types = 1);
 
 namespace Rentpost\ForteApi\Client\SubResource;
 
+use Rentpost\ForteApi\Attribute\Id\LocationId;
+use Rentpost\ForteApi\Attribute\Id\OrganizationId;
+use Rentpost\ForteApi\Attribute\Id\TransactionId;
 use Rentpost\ForteApi\Filter\SettlementFilter;
 use Rentpost\ForteApi\Model;
 use Rentpost\ForteApi\UriBuilder\PaginationData;
@@ -29,6 +32,27 @@ class SettlementSubResource extends AbstractSubResource
     ): Model\SettlementCollection
     {
         $uri = UriBuilder::build('settlements/', [], $filter, $pagination);
+
+        return $this->getHttpClient()->makeModelRequest('get', $uri, Model\SettlementCollection::class);
+    }
+
+
+    /**
+     * Find for the organization which is authenticated
+     *
+     * @param SettlementFilter $filter          Unlike most other find() methods, this one requires filter
+     */
+    public function findForTransactionId(
+        OrganizationId $organizationId,
+        LocationId $locationId,
+        TransactionId $transactionId,
+    ): Model\SettlementCollection
+    {
+        $uri = UriBuilder::build("organizations/%s/locations/%s/transactions/%s/settlements/", [
+            $organizationId->getValue(),
+            $locationId->getValue(),
+            $transactionId->getValue()
+        ]);
 
         return $this->getHttpClient()->makeModelRequest('get', $uri, Model\SettlementCollection::class);
     }
